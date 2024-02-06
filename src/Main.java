0812e16e38c8d69342e6ed84a3d5a2e6bb7a1843
Viktor <viktor.kolar007@email.cz>
2024-02-06 18:13:05 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws OblibenostException {
+        SpravceDeskovek spravceDeskovek = new SpravceDeskovek();
+        spravceDeskovek.pridejDeskovku(new Deskovka("Krycí jména", true, 2));
+        GUI gui = new GUI(spravceDeskovek);
+        gui.setVisible(true);
+    }
+}
